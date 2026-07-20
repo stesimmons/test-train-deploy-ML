8cedae7bf44eca8828d26677617a15a04df9f03e
Simmons <steven.simmons@unisys.com>
2026-07-20 03:30:28 -0700

    reduce Food-101 model size

--- app/model.py
+++ app/model.py
@@ -7,38 +7,58 @@ class Net(nn.Module):
         super().__init__()
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+
+            nn.Conv2d(
+                3,
+                16,
+                kernel_size=3,
+                padding=1
+            ),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.Conv2d(
+                16,
+                32,
+                kernel_size=3,
+                padding=1
+            ),
             nn.ReLU(),
             nn.MaxPool2d(2),
 
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.Conv2d(
+                32,
+                64,
+                kernel_size=3,
+                padding=1
+            ),
             nn.ReLU(),
-            nn.MaxPool2d(2),
+            nn.MaxPool2d(2)
         )
 
         self.classifier = nn.Sequential(
+
             nn.Flatten(),
 
             nn.Linear(
-                128 * 16 * 16,
-                512
+                64 * 16 * 16,
+                128
             ),
 
             nn.ReLU(),
 
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
 
             nn.Linear(
-                512,
+                128,
                 101
             )
         )
 
     def forward(self, x):
+
         x = self.features(x)
+
         x = self.classifier(x)
+
         return x
